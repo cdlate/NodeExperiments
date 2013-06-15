7611612e4b1a2b45c05d111d368f28d94a1b6e01
Alberto <cdlate@gmail.com>
2013-06-15 15:31:01 +1000

    Initial commit

---
+++ stuff/server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var server = express.createServer();
+server.configure(function(){
+    server.use('/media', express.static(__dirname + '/media'));
+    server.use(express.static(__dirname + '/public'));
+});
+server.listen(3000);
