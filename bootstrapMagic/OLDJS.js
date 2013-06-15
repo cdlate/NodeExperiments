c86c773313f7fcc24ad6de8af5741962f8b6a7cf
Alberto <cdlate@gmail.com>
2013-06-15 16:10:13 +1000

    Cleaning Up

---
+++ bootstrapMagic/OLDJS.js
@@ -0,0 +1,34 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Alby
+ * Date: 9/06/13
+ * Time: 8:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+/**
+ * Module dependencies.
+ */
+// bodyParser in connect 2.x uses node-formidable to parse
+// the multipart form data.
+/*
+ app.get('/', function(req, res){
+
+
+ res.send('<form method="post" enctype="multipart/form-data">'
+ + '<p>Title: <input type="text" name="title" /></p>'
+ + '<p>Image: <input type="file" name="image" /></p>'
+ + '<p><input type="submit" value="Upload" /></p>'
+ + '</form>');
+
+ });
+ */
+/*
+ // the uploaded file can be found as `req.files.image` and the
+ // title field as `req.body.title`
+
+ res.send(format('\nuploaded %s (%d Kb) to %s as %s'
+ , req.files.image.name
+ , req.files.image.size / 1024 | 0
+ , req.files.image.path
+ , req.body.title));'../node_modules/
+ */
